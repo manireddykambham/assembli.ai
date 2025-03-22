76e30be07aed93ad74fda90322d72a77021bea64
Mani Shankar <manishankar377@gmail.com>
2025-03-21 21:27:40 -0500

    intial commit

---
+++ src/test/java/com/mani/assembli/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mani.assembli.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
